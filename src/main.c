579629dea21dbbb26cc9c42c4f1f1022c7270144
Ryo Nakamura <upa@haeena.net>
2022-10-15 22:19:40 +0900

    update usage

--- src/main.c
+++ src/main.c
@@ -9,31 +9,37 @@
 #include <platform.h>
 
 
-#define DEFAULT_MIN_CHUNK_SZ      (2 << 20)       /* 2MB */
+#define DEFAULT_MIN_CHUNK_SZ      (64 << 20)       /* 64MB */
 
 void usage(bool print_help) {
         printf("sscp: super scp, copy files over multiple ssh connections\n"
                "\n"
                "Usage: sscp [rvC] [-n max_conns] [-s min_chunk_sz] [-S max_chunk_sz]\n"
                "            [-l login_name] [-p port] [-i identity_file]\n"
-               "            [-c cipher_spec] source ... target\n"
+               "            [-c cipher_spec] source ... target_directory\n"
                "\n");
                
         if (!print_help)
                 return;
 
-        printf("    -r                 copy directory recusrively\n"
+        printf("    -r                 expand directory recusrively\n"
                "    -n NR_CONNECTIONS  max number of connections (default: # of cpu cores)\n"
-               "    -s CHUNKSIZE       min chunk size (default: 2MB)\n"
-               "    -S CHUNKSIZE       max chunk size (default: filesize / nr_conn)\n"
+               "    -s MIN_CHUNKSIZE   min chunk size (default: 64MB)\n"
+               "    -S MAX_CHUNKSIZE   max chunk size (default: filesize / nr_conn)\n"
                "\n"
                "    -l LOGIN_NAME      login name\n"
                "    -p PORT            port number\n"
-               "    -i IDENTITY        identity to be used for ssh\n"
+               "    -i IDENTITY        identity file for publickey authentication\n"
                "    -c CIPHER          cipher spec, see `ssh -Q cipher`\n"
                "    -C                 enable compression on libssh\n"
                "    -v                 increment output level\n"
                "    -h                 print this help\n"
+               "\n");
+
+        printf("  Note:\n"
+               "    Not similar to scp and rsync, target in sscp must be directory\n"
+               "    (at present). This means that sscp cannot change file names.\n"
+               "    sscp copies file(s) into a directory.\n"
                "\n");
 }
 
